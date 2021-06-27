518d7db9fa58223bf3bab3039f399f485fd0470b
Khalida OUAMAR <khalida.ouamar@outlook.com>
2021-06-27 17:11:18 +0200

    create variables file

--- main.tf
+++ main.tf
@@ -1,16 +1,11 @@
-variable "url" {}
-variable "user" {}
-variable "password" {}
-variable "host" {}
-
 resource "null_resource" "ssh_target" {
 
 
   connection {
     type        = "ssh"
-    user        = "${var.user}"
-    host        = "${var.host}"
-    password    = "${var.password}"
+    user        = var.user
+    host        = var.host
+    password    = var.password
     port        = 29
   }
 
---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "user" {
+  type = string
+  default = "guardian"
+}
+
+variable "host" {
+  type = string
+  default = "51.159.56.117"
+}
+
+variable "password" {
+  type = string
+  default = "hikeandseek"
+}
+
+variable "url" {
+  type = string
+  default = "https://Khalida-OUAMAR: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@github.com/Hike-Seek/ML" 
+}
